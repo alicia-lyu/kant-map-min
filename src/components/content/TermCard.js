de59d23a56f60722fe3038fea4dfef428c5aa00a
alicia-lyu <pkulyuwenhui@gmail.com>
2023-03-23 11:36:42 -0500

    ready to deploy

--- src/components/content/TermCard.js
+++ src/components/content/TermCard.js
@@ -9,18 +9,18 @@ import "./termCard.css"
 function TermCard(props) {
     const lottery = Math.floor(Math.random() * props.sentences.length);
 
-    return <Card className="mb-2">
-        <Link to={`/term/${props.name}`} className="clickable">
-            <Card.Body>
+    return <Card className="clickable mb-2">
+        <Card.Body>
+            <Link to={`/term/${props.name}`}>
                 <Card.Title>{props.name}</Card.Title>
                 <Card.Subtitle>{props.english}</Card.Subtitle>
                 <Card.Text className="mt-2">Significance Index in CPR: {props.weight}; Number of Occurrence in CPR: {props.Nsentence}</Card.Text>
                 <Card.Text className="quote">{props.sentences[lottery]}</Card.Text>
-                <Button as="a" href={props.weblink} target="_blank" rel="noreferrer" className="ms-auto d-block">
-                    Definition in Kant Lexicon <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                </Button>
-            </Card.Body>
-        </Link>
+            </Link>
+            <Button as="a" href={props.weblink} target="_blank" rel="noreferrer" className="ms-auto d-block">
+                Definition in Kant Lexicon <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </Button>
+        </Card.Body>
     </Card>
 }
 
